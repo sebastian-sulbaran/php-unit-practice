25a988275c6802730b3d264dc7ea05f4f28deb17
Sebastian Sulbaran <sebastian.sulbaran.r@gmail.com>
2022-12-15 03:36:20 +0000

    implementing test over model post with virtual attributes using mutators and accessors

---
+++ app/Models/Post.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+class Post extends Model
+{
+    use HasFactory;
+
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = Str::lower($value);
+    }
+
+    public function getSlugAttribute()
+    {
+        return Str::replace(' ', '-', $this->name);
+    }
+
+}
---
+++ tests/Unit/Models/PostTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Unit\Models;
+
+use PHPUnit\Framework\TestCase;
+use App\Models\Post;
+
+class PostTest extends TestCase
+{
+    //Mutator
+    public function test_set_name_in_lowercase()
+    {
+
+        $post = new Post;
+        $post->name = "Proyecto en PHP";
+        $this->assertEquals("proyecto en php", $post->name);
+        //$this->assertTrue(true);
+
+    }
+
+    //Accessor
+    public function test_get_slug()
+    {
+
+        $post = new Post;
+        $post->name = "Proyecto en PHP";
+        $this->assertEquals("proyecto-en-php", $post->slug);
+        //$this->assertTrue(true);
+
+    }
+
+}
