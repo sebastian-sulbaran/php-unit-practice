29a9840371d74ba94d12f6fb7640e476d6b84a9e
Sebastian Sulbaran <sebastian.sulbaran.r@gmail.com>
2022-12-22 05:06:17 +0000

    Lesson 16: implementing tag home test

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view(
+        'welcome',
+        ['tags' => App\Models\Tag::get()]
+    );
 });
 
 Route::get('about', function () {
---
+++ tests/Feature/HomeTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class HomeTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testEmpty()
+    {
+        $this
+            ->get('/')
+            ->assertStatus(200)
+            ->assertSee('No hay etiquetas');
+    }
+}
